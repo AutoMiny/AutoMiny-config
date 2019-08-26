e8410e628695e661e1e5dc32f28fb5ab3c02d43d
Stephan Sundermann <stephansundermann@gmail.com>
2019-08-26 11:51:18 +0200

    Update install scripts

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y librealsense2-dkms librealsense2-utils librealsense2-dev
 sudo rosdep init
 rosdep update
 
-sudo apt install -y zsh git
+sudo apt install -y zsh git build-essential
 # This part is optional to install zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -34,25 +34,14 @@ cd src
 git clone https://github.com/IntelRealSense/realsense-ros.git
 git clone https://github.com/pal-robotics/ddynamic_reconfigure.git
 cd ..
-rosdep install --from-paths . --ignore-src --rosdistro=melodic -y
+#librealsense is not found through packages so add -r to continue on error
+rosdep install --from-paths . --ignore-src --rosdistro=melodic -y -r
 sudo apt install -y clang
 catkin config --cmake-args -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++
-
+catkin build
 echo "source /opt/autominy/catkin_ws/devel/setup.bash" >> ~/.bashrc
 echo "source /opt/autominy/catkin_ws/devel/setup.zsh" >> ~/.zshrc
 cd
-
-# adjust this
-echo -e '### ROS Setup
-#IP=192.168.43.199       # ETHERNET
-IP=192.168.43.130       # WIFI
-
-export ROS_HOSTNAME=$IP
-export ROS_MASTER_URI=http://$IP:11311
-export ROS_CALIBRATION_MARKER_ID=11
-export ROS_CAR_ID=130
-export ROS_GPS_MARKER=16
-' > ros-config.sh
 
 echo "source /home/ros/ros-config.sh" >> ~/.bashrc
 echo "source /home/ros/ros-config.sh" >> ~/.zshrc
@@ -64,9 +53,13 @@ sudo cp -r etc/* /etc
 sudo rm /etc/netplan/01-network-manager-all.yaml
 
 # adjust wireless interfaces
-nano 02-wireless.yaml
+sudo nano /etc/netplan/02-wireless.yaml
 
+cp ros-config.sh ..
 cp autostart.sh ..
 sudo chmod +x ../autostart.sh
 sudo systemctl enable autominy.service
+
+sudo bash -c 'echo "ros ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown" >> /etc/sudoers'
+
 
